a540f2321826188046d69a4e2d06e6804cd9a2fd
Belgacem Khadher <devbekh@gmail.com>
2024-05-09 18:53:26 +0100

    Add fixtures and insert some contries and cities

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,79 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\City;
+use App\Entity\Country;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //France
+        $france = new Country;
+        $france->setName('France');
+
+        $paris = new City;
+        $paris->setName('Paris');
+        $paris->setCountry($france);
+
+        $marseille = new City;
+        $marseille->setName('Marseille');
+        $marseille->setCountry($france);
+
+        $lyon = new City;
+        $lyon->setName('Lyon');
+        $lyon->setCountry($france);
+
+        $manager->persist($france);
+        $manager->persist($paris);
+        $manager->persist($marseille);
+        $manager->persist($lyon);
+
+        //Canada
+        $canada = new Country;
+        $canada->setName('Canada');
+
+        $quebec = new City;
+        $quebec->setName('Québec');
+        $quebec->setCountry($canada);
+
+        $montreal = new City;
+        $montreal->setName('Montreal');
+        $montreal->setCountry($canada);
+
+        $troisRivieres= new City;
+        $troisRivieres->setName('Trois-Rivières');
+        $troisRivieres->setCountry($canada);
+
+        $manager->persist($canada);
+        $manager->persist($quebec);
+        $manager->persist($montreal);
+        $manager->persist($troisRivieres);
+
+        //Tunisia
+        $tunisia = new Country;
+        $tunisia->setName('Tunisia');
+
+        $tunis = new City;
+        $tunis->setName('Tunis');
+        $tunis->setCountry($tunisia);
+
+        $sousse = new City;
+        $sousse->setName('Sousse');
+        $sousse->setCountry($tunisia);
+
+        $sfax= new City;
+        $sfax->setName('Sfax');
+        $sfax->setCountry($tunisia);
+
+        $manager->persist($tunisia);
+        $manager->persist($tunis);
+        $manager->persist($sousse);
+        $manager->persist($sfax);
+
+
 
         $manager->flush();
     }
